dfdc29daa0fb056bed57b273bfc1c4dc5b7055e2
Leonel Chauca <leonelchauca0@gmail.com>
2024-03-15 10:23:41 -0400

    Inicializacion del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <dir>
+        <h1>Donacion Sistema 281</h1>
+      </dir>
+    </>
+  )
+}
+
+export default App
