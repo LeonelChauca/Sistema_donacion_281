be458f6326ef527422077ca539109285f6d47d45
Leonel Chauca <leonelchauca0@gmail.com>
2024-03-15 22:01:23 -0400

    Modifiaciones al apartado preguntas freq

--- src/containers/Preguntas.jsx
+++ src/containers/Preguntas.jsx
@@ -1,7 +1,21 @@
+import Accordion from '@mui/material/Accordion';
+import AccordionActions from '@mui/material/AccordionActions';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Acordeon from '../components/Acordeon';
+import s from '../styles/components/Preguntas.module.css';
 const Preguntas =()=>{
     return (
      <>
-        Preguntas
+        <div>
+            <h2>Preguntas Frecuentes</h2>
+            <hr className={s.hr} />
+            <Acordeon titulo={"¿Cómo puedo realizar una donación?"} respuesta={"Puedes realizar una donación a través de nuestro sitio web utilizando nuestro formulario de donación seguro. También aceptamos donaciones por correo o en persona en nuestras oficinas."}/>
+            <Acordeon titulo={"¿Qué tipos de donaciones aceptan?"} respuesta={"Aceptamos donaciones de dinero, productos y dinero"}/>
+            <Acordeon titulo={"¿Cómo puedo ser voluntario?"} respuesta={"Puedes ser voluntario completando nuestro formulario de voluntariado en línea."}/>
+            <Acordeon titulo={"¿Qué tipo de ayuda necesitan?"} respuesta={"Necesitamos ayuda en una variedad de áreas, Pero mas se necesitan en envio y recepcion de donaciones"}/>
+        </div>
      </>
     )
  }
