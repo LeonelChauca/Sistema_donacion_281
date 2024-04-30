329ed30b7514dd4f2c6ff8164b9913e26d626b04
Leonel Chauca <71529078+LeonelChauca@users.noreply.github.com>
2024-04-30 02:01:28 -0400

    terminado

--- src/components/PersonaVoluntarioRegister.jsx
+++ src/components/PersonaVoluntarioRegister.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import style from '../styles/components/PersonaRegister.module.css';
 import { Grid, TextField } from '@mui/material';
 import  Mapa  from './Mapa';
@@ -8,30 +8,40 @@ import OutlinedInput from '@mui/material/OutlinedInput'
 import 'animate.css';
 const PersonaVoluntarioRegister = ({register}) => {
   const [selectT, setselectT] = useState('');
+  const [horario, sethorario] = useState('');
     const handleChange = (event) => {
         setselectT(event.target.value);
+        switch(event.target.value){
+          case "Manana":
+            sethorario("8:00 - 12:00")
+            break;
+          case "Tarde":
+            sethorario("12:00 - 16:00")
+            break;
+          case "Noche":
+            sethorario("16:00 - 20:00")
+            break;
+        }
     };
   return (
     <div>
         <h4>Datos del Voluntario</h4>
         <div className={' animate__animated animate__fadeInDown '+style.container} >
-            <TextField className={style.validar}  {...register("horario")} label="Horario" variant="outlined" 
-            required
-            />
-            
             <Select 
-                    displayEmpty                    
-                    
-                    defaultValue={"Manana"}
-                    
+                    displayEmpty         
+                    defaultValue={""}
                     {...register("turno")}
                     onChange={handleChange}
-          
                     >
                     <MenuItem value={"Manana"}>Mañana</MenuItem>
                     <MenuItem value={"Tarde"}>Tarde</MenuItem>
                     <MenuItem value={"Noche"}>Noche</MenuItem>
-                </Select>
+            </Select>
+            {
+              <TextField className={style.validar}  {...register("horario")} label="Horario" variant="outlined" 
+              required value={horario}
+              />
+            }
         </div>
     </div>
   )
